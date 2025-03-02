efcd550755e44af1f3a8489cf0b9e1bf4b216cec
d-e-v-14 <mehandrudev@gmail.com>
2025-03-02 23:53:07 +0530

    Added the pop function

--- main.c
+++ main.c
@@ -7,28 +7,49 @@ struct node{
     
 };
 
-void push(struct node *tail,int value){
+void push(struct node *head,int value){
     
     struct node *temp=(struct node*)malloc(sizeof(node));
     
     temp->value=value;
-    temp->link=tail;
-    tail=temp;
-    printf("added");
+    temp->link=head;
+    head=temp;
+    printf("\nAdded");
     
 }
 
+struct node *pop(struct node *head){
+    
+
+    if(head==NULL){
+        printf("\nThe list is empty");
+    }else{
+        struct node *temp=(struct node*)malloc(sizeof(node));
+        temp=head;
+        head=head->link;
+        free(temp);
+        printf("\nElement popped");
+    
+    }
+    
+    return head;
+    
+}
+
+
 int main(){
     
-    struct node *tail;
-    tail=(struct node*)malloc(sizeof(node));
+    struct node *head;
+    head=(struct node*)malloc(sizeof(node));
     
-    tail->value=0;
-    tail->link=NULL;
+    head->value=0;
+    head->link=NULL;
     
     int val;
     scanf("%d",&val);
-    push(tail,val);
+    push(head,val);
+    
+    head=pop(head);
     
     
     
