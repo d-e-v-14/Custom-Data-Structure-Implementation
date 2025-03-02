3810f78354b6ebccec6fa926210a45cd024cff41
d-e-v-14 <mehandrudev@gmail.com>
2025-03-02 23:39:59 +0530

    added push function

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
-#include<Stdlib.h>
+#include<stdlib.h>
+
+struct node{
+    int value;
+    struct node *link;
+    
+};
+
+void push(struct node *tail,int value){
+    
+    struct node *temp=(struct node*)malloc(sizeof(node));
+    
+    temp->value=value;
+    temp->link=tail;
+    tail=temp;
+    printf("added");
+    
+}
 
 int main(){
+    
+    struct node *tail;
+    tail=(struct node*)malloc(sizeof(node));
+    
+    tail->value=0;
+    tail->link=NULL;
+    
+    int val;
+    scanf("%d",&val);
+    push(tail,val);
+    
+    
+    
     
 }
